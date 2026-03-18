79bb7b0a4a19c08426aa6bcfeae02c5043523159
aswinramaraj <ashwinramaraj682005@gmail.com>
2026-03-18 12:20:33 +0530

    correct the api url

--- supabase/src/utils/api.js
+++ supabase/src/utils/api.js
@@ -1,6 +1,8 @@
 import axios from 'axios'
 
-const API_ROOT = 'https://smartagri2.onrender.com/api' || 'http://localhost:5000/api'
+const API_ROOT = import.meta.env.VITE_API_URL
+  ? `${import.meta.env.VITE_API_URL}/api`
+  : 'http://localhost:5000/api'
 
 const api = axios.create({
   baseURL: API_ROOT,
